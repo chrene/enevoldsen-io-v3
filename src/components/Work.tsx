545f039fc281b7246eae1021b7f9c0baef158d74
chrene <chrene91@gmail.com>
2023-07-08 00:17:16 +0200

    projects

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,22 @@
-import Container from "./Container";
+import Container from './Container';
+
+const projects = [
+  {
+    title: 'Project 1',
+  },
+  {
+    title: 'Project 2',
+  },
+  {
+    title: 'Project 3',
+  },
+  {
+    title: 'Project 4',
+  },
+  {
+    title: 'Project 5',
+  },
+];
 
 export default function Work() {
   return (
@@ -7,9 +25,11 @@ export default function Work() {
         <div className="py-32 px-8">
           <h2 className="font-medium text-5xl">Det har jeg lavet</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16">
-            <div className="bg-white rounded-lg shadow-lg">
-              <div className="p-8"></div>
-            </div>
+            {projects.map((project) => (
+              <div className="bg-white rounded-lg shadow-lg">
+                <div className="p-8">{project.title}</div>
+              </div>
+            ))}
           </div>
         </div>
       </Container>
