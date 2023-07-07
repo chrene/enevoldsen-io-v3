60a5f8ea0f7010ac2106e5513bf64dcc56eded1f
chrene <chrene91@gmail.com>
2023-07-08 00:05:39 +0200

    add work items

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
-import Container from "./Container";
+import Container from './Container';
+import { projects } from '../data/projects';
 
 export default function Work() {
   return (
@@ -8,7 +9,11 @@ export default function Work() {
           <h2 className="font-medium text-5xl">Det har jeg lavet</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16">
             <div className="bg-white rounded-lg shadow-lg">
-              <div className="p-8"></div>
+              <div className="p-8">
+                {projects.map((project) => (
+                  <div key={project.title}>{project.title}</div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
---
+++ src/data/projects.tsx
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: 'Sofacompany',
+    description:
+      'Sofacompany er en dansk virksomhed, der designer og producerer møbler i høj kvalitet til fornuftige priser. Jeg har været med til at udvikle deres eCommerce platform, der er baseret på Magento 2.',
+    url: 'https://sofacompany.com',
+  },
+  {
+    title: 'YourLocal',
+    description:
+      'YourLocal er en dansk virksomhed, der har udviklet en platform til at bestille takeaway fra lokale restauranter. Jeg har været med til at udvikle deres platform, der er baseret på React og Node.js.',
+    url: 'https://yourlocal.org',
+  },
+  {
+    title: 'Energiteknik Lolland',
+    description:
+      'Energiteknik Lolland er en dansk virksomhed, der sælger og installerer varmepumper. Jeg har været med til at udvikle deres hjemmeside, der er baseret på Gatsby og Sanity.',
+    url: 'https://energitekniklolland.dk',
+  },
+  {
+    title: 'Klaskefar',
+    description:
+      'Energiteknik Lolland er en dansk virksomhed, der sælger og installerer varmepumper. Jeg har været med til at udvikle deres hjemmeside, der er baseret på Gatsby og Sanity.',
+    url: 'https://energitekniklolland.dk',
+  },
+];
