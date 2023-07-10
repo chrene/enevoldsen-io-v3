bf55df9a9bba9883dfc59f02d8fbd164e53868c1
chrene <chrene91@gmail.com>
2023-07-10 22:51:03 +0200

    add critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), critters()]
 });
