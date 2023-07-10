d9196607f3391a22b73266e018b6737d8a8803d0
chrene <chrene91@gmail.com>
2023-07-10 22:56:49 +0200

    Revert "add critical css"
    This reverts commit bf55df9a9bba9883dfc59f02d8fbd164e53868c1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import critters from "astro-critters";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind(), critters()]
+  integrations: [react(), tailwind()]
 });
