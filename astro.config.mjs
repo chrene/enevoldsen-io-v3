508fea4d64818db2cdcedf6e252234a0b5920595
chrene <chrene91@gmail.com>
2023-07-11 11:37:03 +0200

    add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
